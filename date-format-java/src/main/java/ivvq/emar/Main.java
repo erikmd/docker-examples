fc465b4bacd95f2e9217c1cbcc373c8e7bbbf182
Erik Martin-Dorel <erik.martin-dorel@irit.fr>
2019-02-14 00:19:36 +0100

    chore: Remove servlet prefix

--- date-format-java/src/main/java/ivvq/emar/Main.java
+++ date-format-java/src/main/java/ivvq/emar/Main.java
@@ -13,7 +13,7 @@ import java.net.URI;
  */
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8080/date-format/";
+    public static final String BASE_URI = "http://localhost:8080/";
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
