fef8738d5b8d7bb60322b2fe6b489281da624ecb
Erik Martin-Dorel <erik.martin-dorel@irit.fr>
2019-10-01 00:25:55 +0200

    Update bin/install-docker.sh

--- bin/install-docker.sh
+++ bin/install-docker.sh
@@ -9,7 +9,7 @@ sudo apt-get install \
   apt-transport-https \
   ca-certificates \
   curl \
-  gnupg2 \
+  gnupg-agent \
   software-properties-common
 curl -fsSL \
   https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
@@ -20,4 +20,4 @@ sudo add-apt-repository \
   "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
   $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get install docker-ce docker-ce-cli containerd.io
